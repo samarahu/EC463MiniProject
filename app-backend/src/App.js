56b2359cd09cb7d558f8614729fedbfd381802f2
Sam <samarah.u@hotmail.com>
2021-09-09 07:00:34 +0100

    Added nutritional info

--- app-backend/src/App.js
+++ app-backend/src/App.js
@@ -12,25 +12,6 @@ class App extends Component {
         contacts: []
     };
 
-    /*componentDidMount() {
-        // POST request using fetch with set headers
-        const requestOptions = {
-            method: 'POST',
-            headers: { 
-                'Content-Type': 'application/json',
-                'Authorization': 'Bearer my-token',
-                'My-Custom-Header': 'foobar'
-            },
-            body: JSON.stringify({ title: 'React POST Request to FDC API' })
-        };
-        fetch('https://api.nal.usda.gov/fdc/v1/foods/search?query=apple&pageSize=2&api_key=xPgFESAz09jEJa1gt2gJdomzjJU7en8CGCQQNA5a', requestOptions)
-            .then(response => response.json())
-            .then((data) => {
-                this.setState({ contacts: data });
-            })
-            .catch(console.log)
-    }*/
-
     componentDidMount() {
         fetch('https://api.nal.usda.gov/fdc/v1/foods/search?query=apple&pageSize=1&api_key=xPgFESAz09jEJa1gt2gJdomzjJU7en8CGCQQNA5a')
             .then(res => res.json())
--- app-backend/src/components/contacts.js
+++ app-backend/src/components/contacts.js
@@ -11,13 +11,13 @@ const Contacts = ({ contacts }) => {
           <div class="card-body">
             <h5 class="card-title">{contact.description}</h5>
             <h6 class="card-subtitle mb-2 text-muted">{"Nutritional Information:"}</h6>
-            <p class="card-text">{contact.foodNutrients[0].nutrientName + ": " + contact.foodNutrients[0].nutrientNumber + " " + contact.foodNutrients[0].unitName}</p>
-            <p class="card-text">{contact.foodNutrients[1].nutrientName + ": " + contact.foodNutrients[1].nutrientNumber + " " + contact.foodNutrients[1].unitName}</p>
-            <p class="card-text">{contact.foodNutrients[2].nutrientName + ": " + contact.foodNutrients[2].nutrientNumber + " " + contact.foodNutrients[2].unitName}</p>
-            <p class="card-text">{contact.foodNutrients[3].nutrientName + ": " + contact.foodNutrients[3].nutrientNumber + " " + contact.foodNutrients[3].unitName}</p>
-            <p class="card-text">{contact.foodNutrients[4].nutrientName + ": " + contact.foodNutrients[4].nutrientNumber + " " + contact.foodNutrients[4].unitName}</p>
-            <p class="card-text">{contact.foodNutrients[5].nutrientName + ": " + contact.foodNutrients[5].nutrientNumber + " " + contact.foodNutrients[5].unitName}</p>
-            <p class="card-text">{contact.foodNutrients[6].nutrientName + ": " + contact.foodNutrients[6].nutrientNumber + " " + contact.foodNutrients[6].unitName}</p> 
+            <p class="card-text">{contact.foodNutrients[0].nutrientName + " - " + contact.foodNutrients[0].nutrientNumber + " " + contact.foodNutrients[0].unitName}</p>
+            <p class="card-text">{contact.foodNutrients[1].nutrientName + " - " + contact.foodNutrients[1].nutrientNumber + " " + contact.foodNutrients[1].unitName}</p>
+            <p class="card-text">{contact.foodNutrients[2].nutrientName + " - " + contact.foodNutrients[2].nutrientNumber + " " + contact.foodNutrients[2].unitName}</p>
+            <p class="card-text">{contact.foodNutrients[3].nutrientName + " - " + contact.foodNutrients[3].nutrientNumber + " " + contact.foodNutrients[3].unitName}</p>
+            <p class="card-text">{contact.foodNutrients[4].nutrientName + " - " + contact.foodNutrients[4].nutrientNumber + " " + contact.foodNutrients[4].unitName}</p>
+            <p class="card-text">{contact.foodNutrients[5].nutrientName + " - " + contact.foodNutrients[5].nutrientNumber + " " + contact.foodNutrients[5].unitName}</p>
+            <p class="card-text">{contact.foodNutrients[6].nutrientName + " - " + contact.foodNutrients[6].nutrientNumber + " " + contact.foodNutrients[6].unitName}</p> 
           </div>
         </div>
       ))}
